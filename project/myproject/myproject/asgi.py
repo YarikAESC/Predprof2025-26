d75acf5203499b87b37378951da4f5975b9a4e66
Xalo <o.kelaskina2009@gmail.com>
2026-01-16 20:29:30 +0300

    Update asgi.py

--- project/myproject/myproject/asgi.py
+++ project/myproject/myproject/asgi.py
@@ -1,16 +1,11 @@
-"""
-ASGI config for myproject project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
+# Настройка ASGI для проекта myproject
 
 import os
 
 from django.core.asgi import get_asgi_application
 
+# Устанавливаем настройки проекта
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
+# Создаем ASGI приложение
 application = get_asgi_application()
