0d2b23a48fde7ccaf9bfc4f73c014271e413a826
Xalo <o.kelaskina2009@gmail.com>
2026-01-16 20:24:29 +0300

    Update apps.py

--- project/myproject/orders/apps.py
+++ project/myproject/orders/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
-
+# Настройки приложения заказы
 class OrdersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'orders'
-    verbose_name = 'Заказы'
+    default_auto_field = 'django.db.models.BigAutoField'  # Стандартный ID поля
+    name = 'orders'  # Название приложения
+    verbose_name = 'Заказы'  # Имя в админке
