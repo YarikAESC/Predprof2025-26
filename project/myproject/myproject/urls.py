342c1e78d1dc3e87587997a5ae5f4f5271aa3de3
Xalo <o.kelaskina2009@gmail.com>
2026-01-16 20:27:14 +0300

    Update urls.py

--- project/myproject/myproject/urls.py
+++ project/myproject/myproject/urls.py
@@ -3,13 +3,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Список URL-адресов проекта
 urlpatterns = [
-    path('', include('orders.urls')),
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    
+    path('', include('orders.urls')),  # Главная страница - приложение orders
+    path('admin/', admin.site.urls),   # Админка Django
+    path('users/', include('users.urls')),  # Приложение пользователей
 ]
-# Для обслуживания медиа файлов в разработке
+
+# Добавляем обработку файлов в режиме разработки
 if settings.DEBUG:
+    # Отдаем файлы из папки media
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Отдаем статические файлы
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
