a9b53f93d981dac21c76bec1a171659eadf02682
Xalo <o.kelaskina2009@gmail.com>
2026-01-16 20:26:40 +0300

    Update wsgi.py

--- project/myproject/myproject/wsgi.py
+++ project/myproject/myproject/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for myproject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+# Настройка WSGI для проекта myproject
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
+# Указываем настройки проекта
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
+# Создаем WSGI приложение
 application = get_wsgi_application()
