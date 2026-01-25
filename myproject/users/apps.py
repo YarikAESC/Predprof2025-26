5809f2b4f678cef641f45aedd00e39f73e1e9b70
Xalo <o.kelaskina2009@gmail.com>
2026-01-25 22:23:08 +0300

    Delete myproject directory

--- myproject/users/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-# Конфигурация приложения users
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'  # Стандартное поле ID
-    name = 'users'  # Название приложения
